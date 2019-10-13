99a9d63fd562dfe16672ac3c362ccd65fbb2036d
Rayffer <oscar.guillamon@gmail.com>
2019-10-13 13:02:48 +0200

    1. Remove proof of concept code from executable project

--- TextSearchEngine/Program.cs
+++ TextSearchEngine/Program.cs
@@ -1,29 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TextSearchEngine
+﻿namespace TextSearchEngine
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var directoryFiles = System.IO.Directory.GetFiles(args[0]).ToList();
-            Console.WriteLine("Introduce text to search:");
-            string searchTerm = Console.ReadLine();
-            var searchedFiles = directoryFiles.Select(file =>
-            {
-                return (fileName: file, occurrences: TextFileSearcher.SearchTextInFile(file, searchTerm));
-            }).OrderByDescending(searchedFile => searchedFile.occurrences).Take(10).TakeWhile(searchedFile => searchedFile.occurrences > 0).ToList();
-            searchedFiles.ForEach(searchedFile =>
-            {
-                Console.WriteLine(string.Format("{0} => occurrences: {1}", Path.GetFileName(searchedFile.fileName), searchedFile.occurrences));
-            });
-            Console.WriteLine("Search finished, press any key to end the program");
-            Console.ReadKey();
         }
     }
 }
