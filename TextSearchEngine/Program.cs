ddb416c0f76590bb601f50b6d0019726e2cc6511
Rayffer <oscar.guillamon@gmail.com>
2019-10-13 12:09:47 +0200

    1. Added file to perform text searchs in a file as a proof of concept.

--- TextSearchEngine/Program.cs
+++ TextSearchEngine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace TextSearchEngine
     {
         static void Main(string[] args)
         {
+            var directoryFiles = System.IO.Directory.GetFiles(args[0]).ToList();
+            Console.WriteLine("Introduce text to search:");
+            string searchTerm = Console.ReadLine();
+            var searchedFiles = directoryFiles.Select(file =>
+            {
+                return (fileName: file, occurrences: TextFileSearcher.SearchTextInFile(file, searchTerm));
+            }).OrderByDescending(searchedFile => searchedFile.occurrences).Take(10).TakeWhile(searchedFile => searchedFile.occurrences > 0).ToList();
+            searchedFiles.ForEach(searchedFile =>
+            {
+                Console.WriteLine(string.Format("{0} => occurrences: {1}", Path.GetFileName(searchedFile.fileName), searchedFile.occurrences));
+            });
+            Console.WriteLine("Search finished, press any key to end the program");
+            Console.ReadKey();
         }
     }
 }
---
+++ TextSearchEngine/TextFileSearcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TextSearchEngine
+{
+    public static class TextFileSearcher
+    {
+        public static int SearchTextInFile(string fileToSearchIn, string searchText)
+        {
+            string fileContent = System.IO.File.ReadAllText(fileToSearchIn);
+            return Regex.Matches(fileContent, searchText).Count;
+        }
+    }
+}
